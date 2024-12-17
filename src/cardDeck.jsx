6af1de07107bca16b459cfbd780a6fc6a56b0ff6
Stephens37 <97484915+Stephens37@users.noreply.github.com>
2024-12-17 05:05:32 -0600

    'Find that checkDuplicates not returning'

--- src/cardDeck.jsx
+++ src/cardDeck.jsx
@@ -3,7 +3,7 @@ import Card from './card.jsx'
 import { useEffect, useState } from 'react'
 
 export default function CardDeck () {
-    const [thesePokeArr, setThesePokeArr] = useState([])
+    let thesePokeArr = []
 
     const pokemonArr = ['pikachu', 'squirtle', 'bulbasaur', 'charmander', 'pidgey', 'mudkip', 'quilava','cyndaquil', 'chikorita', 'totodile', 'ditto', 'mew', 'arceus', 'ho-oh', 'suicune', 'reshiram', 'zekrom', 'raichu', 'pichu', 'magikarp', 'gyarados', 'cubone']
     const [cards, setCards] = useState(Array(8).fill(''))
@@ -14,36 +14,44 @@ export default function CardDeck () {
 
     function checkDuplicates(checkDupName, curArr) {
         for (let i = 0; i < curArr.length; i++) {
-            let j = 0
-            j++
-            console.log(j)
+            console.log(i)
             if (checkDupName === curArr[i].key) {
+                console.log(checkDupName)
+                console.log(curArr[i].key)
                 const newName = pokemonArr[getRandomInt(pokemonArr.length)]
+                console.log(newName)
                 checkDuplicates(newName, thesePokeArr)
+            } else {
+                console.log(checkDupName)
+                return checkDupName
             }
         }
-        return checkDupName
     }
 
     function checkInputName(inputedName) {
             if (thesePokeArr.length === 0) {
                 thesePokeArr.push({name: inputedName, key: inputedName})
-                return inputedName
+                console.log(thesePokeArr)
+                return {thesePokeArr, inputedName}
             } else {
                     inputedName = checkDuplicates(inputedName, thesePokeArr)
-                    if (thesePokeArr.length === 8) {
-                        setThesePokeArr([])
-                        return inputedName
-                    } else if (thesePokeArr.length < 8) {
+                    console.log(inputedName)
+                    if (thesePokeArr.length === 7) {
+                        thesePokeArr = []
+                        console.log(thesePokeArr)
+                        return {thesePokeArr, inputedName}
+                    } else if (thesePokeArr.length < 7) {
+                        console.log(thesePokeArr)
                         thesePokeArr.push({name: inputedName, key: inputedName})
-                        return inputedName
+                        return {thesePokeArr, inputedName}
                     }
                 }
             }
     function deckSet () {
         const newSet = cards.map(() => {
             const randomName = pokemonArr[getRandomInt(pokemonArr.length)]
-            const thisCardName = checkInputName(randomName)
+            console.log(thesePokeArr)
+            const thisCardName = checkInputName(randomName).inputedName
             
             return <Card pokeName={thisCardName} onClick={deckSet} key={thisCardName}></Card>
     })
