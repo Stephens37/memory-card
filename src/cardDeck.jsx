bb6b6e77f563f422f3693ed35fe8a7606322f4e1
Stephens37 <97484915+Stephens37@users.noreply.github.com>
2024-12-23 14:57:05 -0600

    'Delete raichu; remove state logic from clickDeck'

--- src/cardDeck.jsx
+++ src/cardDeck.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState, useRef } from 'react'
 export default function CardDeck () {
     let thesePokeArr = []
 
-    const pokemonArr = ['pikachu', 'squirtle', 'bulbasaur', 'charmander', 'pidgey', 'mudkip', 'quilava','cyndaquil', 'chikorita', 'totodile', 'ditto', 'mew', 'arceus', 'ho-oh', 'suicune', 'reshiram', 'zekrom', 'raichu', 'pichu', 'magikarp', 'gyarados', 'cubone']
+    const pokemonArr = ['pikachu', 'squirtle', 'bulbasaur', 'charmander', 'pidgey', 'mudkip', 'quilava','cyndaquil', 'chikorita', 'totodile', 'ditto', 'mew', 'arceus', 'ho-oh', 'suicune', 'reshiram', 'zekrom', 'pichu', 'magikarp', 'gyarados', 'cubone']
     const [cards, setCards] = useState(Array(8).fill(''))
 
     function getRandomInt(max) {
@@ -62,8 +62,6 @@ export default function CardDeck () {
     }, [highScore])
 
     function scoreKeeping (scoreName) {
-        console.log(scoreName)
-        console.log(pokemonClickedArr)
         let curScoreInc = scoreIncRef.current
         let curPokeArr = pokeArrRef.current
         let curHighScore = highScoreRef.current
@@ -92,13 +90,12 @@ export default function CardDeck () {
                         setHighScore(curScoreInc)
                     } setPokemonClickedArr([])
                     setScoreInc((curScoreInc) => curScoreInc - curScoreInc)
-                    console.log(curPokeArr)
-                    console.log(curScoreInc)
                     return
                 } else if (curPokeArr[i] !== scoreName && i === (curPokeArr.length - 1) && scoreInc < 21) {
                     console.log('f')
                     setPokemonClickedArr((curPokeArr) => [...curPokeArr, scoreName])
                     setScoreInc((curScoreInc) => curScoreInc + 1)
+                    console.log(curPokeArr)
                     return
                 } else if (curPokeArr[i] !== scoreName && i === (curPokeArr.length - 1) && scoreInc === 21) {
                     console.log('g')
@@ -111,36 +108,25 @@ export default function CardDeck () {
         }
     }
 
-    const [clickDeckArr, setClickDeckArr] = useState([])
-    const clickDeckRef = useRef(clickDeckArr)
-
-    useEffect (() => {
-        clickDeckRef.current = clickDeckArr
-    }, [clickDeckArr])
-
-
     function deckSet (clickedElement = undefined) {
-        let clickDeckCur = clickDeckRef.current
-        let pokeArrCur = pokeArrRef.current
         scoreKeeping(clickedElement)
+        let pokeArrCur = pokeArrRef.current
         const newSet = cards.map(() => {
             const randomName = pokemonArr[getRandomInt(pokemonArr.length)]
             const thisCardName = checkInputName(randomName).inputedName
             return <Card pokeName={thisCardName} onClick={() => deckSet(thisCardName)} key={thisCardName}></Card>
     })
-    /*
-        - within the deckSet function after orgSet
-        - create const checkIfClicked with an arrow function inside
-        - function starts with a loop which checks over all elements in pokemonArrRef
-        - inside of that loop, a new loop starts which checks over all elements in deckArr
-        - if two elements in each arr are the same, add them to clickDeckArr
-        - if clickDeckArr.length = 8 then run deckSet again
-        - else return deckArr
-        - setCards will then take checkIfClicked as a parameter
-    */
 
+    /*
+        - pokeArrCur is not updating in time for the checkIfClicked function to recognize the latest clicked pokemon
+        
+        - could be because scoreKeeping is being called after let pokeArrCur (nope, still not showing the last pokemon)
+
+        - need to be able to access the last array element before CardDeck rerenders
+    */
     const checkIfClicked = () => {
         console.log(pokeArrCur)
+        let clickDeckCur = []
         if(pokeArrCur.length === 0) {
             return newSet
         } else {
@@ -150,7 +136,7 @@ export default function CardDeck () {
                     if (pokeArrCur[i] === newSet[j].key) {
                         console.log('dd')
                         console.log(newSet[j].key)
-                        setClickDeckArr((clickDeckCur) => [...clickDeckCur, newSet[j].key])
+                        clickDeckCur.push(newSet[j].key)
                         console.log(clickDeckCur)
                     } else if (clickDeckCur.length === 8) {
                         console.log('ee')
@@ -159,7 +145,7 @@ export default function CardDeck () {
                     } else if (clickDeckCur.length < 8 && i === pokeArrCur.length - 1) {
                         console.log('ff')
                         console.log(clickDeckCur)
-                        setClickDeckArr([])
+                        clickDeckCur = []
                         return newSet
                     }
                 }
@@ -184,12 +170,3 @@ export default function CardDeck () {
         </>
     )
 }
-
-/*
-    create random number function
-    create onClick function
-    loops the pokemonArr 8 times when triggered
-    each iteration will pull the card component and set its name to be pokemonArr.random
-    card[i] (i being iteration) will have it's state set to the new card
-    repeat
-*/
