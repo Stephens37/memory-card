7bea5dcb75a609358e9e5e8afe4c83ecfb1175a8
Stephens37 <97484915+Stephens37@users.noreply.github.com>
2024-12-27 10:20:37 -0600

    'Syntax changes; remove console.logs/comments'

--- src/cardDeck.jsx
+++ src/cardDeck.jsx
@@ -69,51 +69,33 @@ export default function CardDeck () {
     }, [highScore])
 
     function scoreKeeping (scoreName) {
-        let curTopText = topTextRef.current
         let curScoreInc = scoreIncRef.current
         let curPokeArr = pokeArrRef.current
         let curHighScore = highScoreRef.current
-        console.log(curScoreInc)
         if (curPokeArr.length === 0 && highScore === 0 && curScoreInc === -1) {
-            console.log('a')
             setScoreInc((curScoreInc) => curScoreInc + 1)
-            console.log(curScoreInc)
             return
         } else if (curPokeArr.length === 0 && curScoreInc === 0 && scoreName !== undefined) {
-            console.log('b')
             setTopText(() => "Get points by clicking on an image but don't click on any more than once!")
             setPokemonClickedArr([scoreName])
             setScoreInc((curScoreInc) => curScoreInc + 1)
-            console.log(scoreInc)
             return
         } else {
-            console.log(curPokeArr)
             for (let i = 0; i < curPokeArr.length; i++) {
                 if (curPokeArr[i] === scoreName) {
-                    console.log('d')
                     if (curHighScore < curScoreInc) {
-                        console.log('e')
                         setHighScore(curScoreInc)
                     } 
                     setTopText(() => 'Try Again')
-                    console.log(curTopText)
                     setPokemonClickedArr([])
                     setScoreInc((curScoreInc) => curScoreInc - curScoreInc)
                     return
                 } else if (curPokeArr[i] !== scoreName && i === (curPokeArr.length - 1) && curScoreInc < 20) {
-                    console.log('f')
-                    console.log(i)
-                    console.log(curScoreInc)
                     setPokemonClickedArr((curPokeArr) => [...curPokeArr, scoreName])
                     setScoreInc((curScoreInc) => curScoreInc + 1)
-                    console.log(curPokeArr)
                     return
                 } else if (curPokeArr[i] !== scoreName && i === (curPokeArr.length - 1) && curScoreInc === 20) {
-                    console.log('g')
-                    console.log('You Win!')
-                    console.log(curScoreInc)
                     setTopText(() => 'Game Over!')
-                    console.log(curTopText)
                     setHighScore((curHighScore) => curScoreInc + 1)
                     setPokemonClickedArr([])
                     setScoreInc((curScoreInc) => curScoreInc - curScoreInc)
@@ -129,68 +111,51 @@ export default function CardDeck () {
             const randomName = pokemonArr[getRandomInt(pokemonArr.length)]
             const thisCardName = checkInputName(randomName).inputedName
             return <Card pokeName={thisCardName} onClick={() => deckSet(thisCardName)} key={thisCardName}></Card>
-    })
+        })
 
-    /*
-        - occasional scoreName showing as undefined, presumable after deckSet triggers again
-        - score (might) still be skipping from 20 to 22, no high score/reset,
-        possible from presentPokeArr loop having one more iteration,
-        possible fix found in just returning func at 21, need to verify
-
-        -score skipping because as deckSet is being called again, scoreKeeping is also called
-    */
-    const checkIfClicked = () => {
-        console.log(pokeArrCur)
-        // regenSet returning same set each time, put it in loop to randomize
-        let clickDeckCur = []
-        let presentPokeArr = [...pokeArrCur, clickedElement]
-        if(presentPokeArr.length === 0) {
-            return clickedNewSet
-        } else {
-            console.log(pokeArrCur)
-            console.log(presentPokeArr)
-            function checkLoop (clickedLoopSet) {
-                for(let i = 0; i <= presentPokeArr.length; i++) {
-                    for(let j = 0; j < clickedLoopSet.length; j++) {
-                        if(newSet === undefined) {
-                            return
-                        }
-                        else if (i === 21) {
-                            newSet === undefined
-                            return newSet
-                        } else if (presentPokeArr[i] === clickedLoopSet[j].key) {
-                            console.log(clickedLoopSet[j].key)
-                            clickDeckCur.push(clickedLoopSet[j].key)
-                        } else if (clickDeckCur.length === 8) {
-                            clickDeckCur = []
-                            const regenSet = cards.map(() => {
-                                const randomName = pokemonArr[getRandomInt(pokemonArr.length)]
-                                const thisCardName = checkInputName(randomName).inputedName
-                                return <Card pokeName={thisCardName} onClick={() => deckSet(thisCardName)} key={thisCardName}></Card>
-                            })
-                            return checkLoop(regenSet)
-                        } else if (clickDeckCur.length < 8 && i === presentPokeArr.length) {
-                            clickDeckCur = []
-                            return clickedLoopSet
+        const checkIfClicked = () => {
+            let clickDeckCur = []
+            let presentPokeArr = [...pokeArrCur, clickedElement]
+            if (presentPokeArr.length === 0) {
+                return clickedNewSet
+            } else {
+                function checkLoop (clickedLoopSet) {
+                    for(let i = 0; i <= presentPokeArr.length; i++) {
+                        for(let j = 0; j < clickedLoopSet.length; j++) {
+                            if(newSet === undefined) {
+                                return
+                            }
+                            else if (i === 21) {
+                                newSet === undefined
+                                return newSet
+                            } else if (presentPokeArr[i] === clickedLoopSet[j].key) {
+                                clickDeckCur.push(clickedLoopSet[j].key)
+                            } else if (clickDeckCur.length === 8) {
+                                clickDeckCur = []
+                                const regenSet = cards.map(() => {
+                                    const randomName = pokemonArr[getRandomInt(pokemonArr.length)]
+                                    const thisCardName = checkInputName(randomName).inputedName
+                                    return <Card pokeName={thisCardName} onClick={() => deckSet(thisCardName)} key={thisCardName}></Card>
+                                })
+                                return checkLoop(regenSet)
+                            } else if (clickDeckCur.length < 8 && i === presentPokeArr.length) {
+                                clickDeckCur = []
+                                return clickedLoopSet
+                            }
                         }
                     }
                 }
+                return checkLoop(newSet)
             }
-            return checkLoop(newSet)
         }
-    }
 
     setCards(checkIfClicked)
-    /*
-        - need to keep testing to ensure checkLoop(regenSet) logic works
-        - stop infinite loop when score reaches 21
-    */
     }
     useEffect(() => {
         deckSet(undefined)
     }, [])
+
     useEffect(() => {
-        console.log('Updated scoreInc:', scoreInc)
     }, [scoreInc])
     
     return(
